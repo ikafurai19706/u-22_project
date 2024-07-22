3d43eb384d346d837e81b5a80dee83da9f3a7192
it0maki_ey <ikafurai19706@gmail.com>
2024-07-22 15:56:05 +0900

    update build.cs

--- src/project/Source/project/project.Build.cs
+++ src/project/Source/project/project.Build.cs
@@ -8,7 +8,7 @@ public class project : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "EditorFramework" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
